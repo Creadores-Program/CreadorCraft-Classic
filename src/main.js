f0ae4f3e6ca933c6afdc8184d6c3563fc6c59170
Trollhunters501PC <cpcreador@gmail.com>
2024-09-28 18:00:14 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -20,3 +20,120 @@ window.addEventListener("load", function(){
     });
   });
 });
+
+function weatherCycleInit() {
+          var hours2 = date.getHours();
+          var weatherStart = Math.random() * (23 - 0) + 0;
+          var weaterStop = Math.random() * (23 - 1) + 1;
+          if (hours2 >= weatherStart && hours2 < weaterStop) {
+            clearInterval(daycycle1);
+            moon.style.visibility = "hidden";
+            sun.style.visibility = "hidden";
+            $(".clouds").hide();
+            $(".clouds1").hide();
+            $(".clouds2").hide();
+            $(".clouds3").hide();
+            $(".clouds4").hide();
+            $(".clouds5").hide();
+            $(".cloudsN").hide();
+            $(".cloudsN1").hide();
+            $(".cloudsN2").hide();
+            $(".cloudsN3").hide();
+            $(".cloudsN4").hide();
+            $(".cloudsN5").hide();
+            $("#starsGame").hide();
+            $(".cloudsW").show();
+            $(".cloudsW1").show();
+            $(".cloudsW2").show();
+            $(".cloudsW3").show();
+            $(".cloudsW4").show();
+            $(".cloudsW5").show();
+            document.body.style.backgroundColor = '#6D6968';
+            setTimeout(function() {
+              document.querySelector(".lightning").style.display = 'block';
+              }, Math.random() * 5000);
+            $('.dropsbackground').show();
+            var dropsfond = document.querySelector(".dropsbackground");
+            const rain = ()=> {
+            let randomdrop = 0;
+              let x = innerWidth * Math.random();
+              let time = 1 * Math.random();
+            while (randomdrop <= 80){
+              var dropsd = document.createElement('drops');
+              drops.style.animationDuration = time + 's';
+              drops.style.animationDelay = time + 's';
+              drops.style.left = x + 'px';
+              dropsfond.appendChild(dropsd);
+              randomdrop++
+              }
+            }
+            rain();
+          }else{
+            daycycle();
+            daycycle1 = setInterval(daycycle, 1000 * 60);
+            $('.dropsbackground').hide();
+            $('.lightning').hide();
+            clearTimeout(function() {
+              document.querySelector(".lightning").style.display = 'block';
+              }, Math.random() * 5000);
+          }
+        }
+function daycycle() {
+          var date = new Date();
+          var hours = date.getMinutes();
+          var sun = document.getElementById("sunGame");
+          var moon = document.getElementById("moonGame");
+          if (hours >= 0 && hours < 30) {
+            document.body.style.backgroundColor = '#00FFFF';
+            moon.style.visibility = "hidden";
+            sun.style.visibility = "visible";
+            $(".clouds").show();
+            $(".clouds1").show();
+            $(".clouds2").show();
+            $(".clouds3").show();
+            $(".clouds4").show();
+            $(".clouds5").show();
+            $(".cloudsN").hide();
+            $(".cloudsN1").hide();
+            $(".cloudsN2").hide();
+            $(".cloudsN3").hide();
+            $(".cloudsN4").hide();
+            $(".cloudsN5").hide();
+            $("#starsGame").hide();
+            $(".cloudsW").hide();
+            $(".cloudsW1").hide();
+            $(".cloudsW2").hide();
+            $(".cloudsW3").hide();
+            $(".cloudsW4").hide();
+            $(".cloudsW5").hide();
+            var sunPosition = (hours - 0) * 10;
+            sun.style.left = sunPosition + "px";
+            
+          } else {
+            document.body.style.backgroundColor = '#0C090A';
+            sun.style.visibility = "hidden";
+            moon.style.visibility = "visible";
+            $(".clouds").hide();
+            $(".clouds1").hide();
+            $(".clouds2").hide();
+            $(".clouds3").hide();
+            $(".clouds4").hide();
+            $(".clouds5").hide();
+            $(".cloudsW").hide();
+            $(".cloudsW1").hide();
+            $(".cloudsW2").hide();
+            $(".cloudsW3").hide();
+            $(".cloudsW4").hide();
+            $(".cloudsW5").hide();
+            $(".cloudsN").show();
+            $(".cloudsN1").show();
+            $(".cloudsN2").show();
+            $(".cloudsN3").show();
+            $(".cloudsN4").show();
+            $(".cloudsN5").show();
+            $("#starsGame").show();
+            $(".starsGame").show();
+            var moonPosition = (hours - 30) * 10;
+            moon.style.right = moonPosition + "px";
+            }
+          }
