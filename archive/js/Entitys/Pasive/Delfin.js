5e9e6c29dd73e1927efc38f7d05ec73eb9c8625f
Enrique <enrique.hernandez@jala.university>
2025-08-18 23:58:24 -0600

    Remove unused item and biome classes, ambient scripts, and associated textures to streamline the project. This includes the deletion of classes for Piedra, Ruby, Tierra, TroncoDeMadera, Day, Weather, Biome, Nevado, Structure, and Generador, along with their respective texture files. Clean up the manifest and main.js to reflect these changes.

--- archive/js/Entitys/Pasive/Delfin.js
+++
@@ -1,3 +0,0 @@
-const Entity = require('../Entity.js');
-class Delfin extends Entity {}
-module.exports = Delfin;
--- archive/js/Terreno/SubBiomesUni/Montaña.js
+++
@@ -1,7 +0,0 @@
-const SubBiomeUni = require('./SubBiomeUni.js');
-class Montaña extends SubBiomeUni {
-    static id = 0;
-    generateTerrain(filtrer){
-    }
-}
-module.exports = Montaña;
